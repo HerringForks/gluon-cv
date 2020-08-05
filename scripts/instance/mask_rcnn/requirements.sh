459761660294104c116c0d4c240cecf34967171c
Karan Jariwala <karankjariwala@gmail.com>
2020-08-05 10:35:01 -0700

    Improved comments

--- scripts/instance/mask_rcnn/requirements.sh
+++ scripts/instance/mask_rcnn/requirements.sh
@@ -1,6 +1,6 @@
 pip install pybind11==2.5.0
 
-# Install NVIDIA COCO and dllogger
+# Install NVIDIA COCO
 pip uninstall -y pycocotools
 pip --no-cache-dir --no-cache install \
     'git+https://github.com/NVIDIA/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
