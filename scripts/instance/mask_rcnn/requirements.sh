54f3cb63597250b63f8aab4eb8ae5f7ee7b77448
Karan Jariwala <karankjariwala@gmail.com>
2020-08-05 12:19:55 -0700

    Added gluoncv stable version as comment

--- scripts/instance/mask_rcnn/requirements.sh
+++ scripts/instance/mask_rcnn/requirements.sh
@@ -5,6 +5,6 @@ pip uninstall -y pycocotools
 pip --no-cache-dir --no-cache install \
     'git+https://github.com/NVIDIA/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
 
-# Install nightly GluonCV for now since changes in Mask RCNN is not compatible with stable pip release
+# Install nightly GluonCV for now since changes in Mask RCNN is not compatible with stable pip release v0.7.0
 pip uninstall -y gluoncv
 pip install gluoncv==0.8.0b20200801
